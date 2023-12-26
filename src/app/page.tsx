46ef228a38e5ea0115251c589cc30595ecbaeb17
Roy Joseph Rimachi Costillas <roy.rimachi@applydigital.com>
2023-12-26 16:01:15 -0500

    fix: refactor app folder

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import HomeLayout from "@/components/layout/home.layout";
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen relative">
+      <HomeLayout />
+    </main>
+  );
+}
---
+++ src/components/header/home-header.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/utils";
+
+export default function HomeHeader() {
+  return (
+    <div
+      className={cn(
+        "w-full  px-0 h-[96px] flex items-center justify-center",
+        "md:justify-between md:px-[30px] md:py-0 md:h-[64px]"
+      )}
+    >
+      <h1>Logo</h1>
+      <a className="md:inline hidden" href="/home">
+        Go to Main Site
+      </a>
+    </div>
+  );
+}
